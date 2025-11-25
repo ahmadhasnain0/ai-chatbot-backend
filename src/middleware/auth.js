0759197764aa716d897d32274c93c17b8073fa13
Usamaa <usamaa.mangocoders@gmail.com>
2025-11-25 19:30:35 +0500

    Apply logout and resolve chat bug

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
   // READ token from HttpOnly cookie
-  const token = req.cookies.token;
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({
